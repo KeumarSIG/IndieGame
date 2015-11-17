55fdb793650047f7915cb10b74d3337dce792ad4
KeumarSIG <m.bonte@K004-5405-01.etudiant.rubika-edu.com.local>
2015-11-17 15:32:35 +0100

    Commit with moving hazards
    - Moving hazards added

---
+++ Assets/Scripts/HazardLinearMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class HazardLinearMovement : MonoBehaviour
+{
+    public Transform m_startPosition;
+    public Transform m_endPosition;
+    public float m_travelTime; 
+	
+	void FixedUpdate ()
+    {
+        Vector3 currentPosition = Vector3.Lerp(m_startPosition.position, m_endPosition.position, Mathf.Cos(Time.time / m_travelTime * Mathf.PI * 2) * -0.5f + 0.5f);
+        GetComponent<Rigidbody>().MovePosition(currentPosition);
+	}
+}
