34d7bb450f3aaa84a9afdc3f4c3b01f93c50c011
KeumarSIG <m.bonte@K004-5405-01.etudiant.rubika-edu.com.local>
2016-01-05 14:23:11 +0100

    Added GUI script from David

---
+++ Assets/Scripts/ScoreTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreTest : MonoBehaviour
+{
+    public Text[] m_scoresOfPlayers = new Text[4];
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0 ; i < 4; i++)
+        {
+            m_scoresOfPlayers[i].text = m_scoresOfPlayers[i].text + " score";
+        }
+	}
+}
