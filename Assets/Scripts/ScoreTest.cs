fabc8c03ca5b9f03424dd58ae8702a263bd5dd4f
KeumarSIG <m.bonte@K004-5405-01.etudiant.rubika-edu.com.local>
2016-01-06 19:46:43 +0100

    Mise à jour proto
    - Menu
    - Image UI
    - Bumpers tweakés
    - écran de victoire

--- Assets/Scripts/ScoreTest.cs
+++ Assets/Scripts/ScoreTest.cs
@@ -4,7 +4,14 @@ using System.Collections;
 
 public class ScoreTest : MonoBehaviour
 {
+    [Header("Current level")]
+    public bool m_currentLevelColorWhite;
+    public Material m_newMaterial;
+
+    [Header("Score")]
     public Text[] m_scoresOfPlayers = new Text[4];
+
+    [Header("Player")]
     public GameObject m_playerOne;
     public GameObject m_playerTwo;
     public GameObject m_playerThree;
@@ -13,15 +20,24 @@ public class ScoreTest : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
+        // Level flipper
+        if (m_currentLevelColorWhite == true)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                m_scoresOfPlayers[i].material = m_newMaterial;
+            }
+        }
+
         int basicHp = m_playerOne.GetComponent<CharacterManagement>().m_currentHealth;
         ScoreUpdatingReturn(basicHp, basicHp, basicHp, basicHp);
 	}
 
     public void ScoreUpdatingReturn(int a, int b, int c, int d)
     {
-        m_scoresOfPlayers[0].text = a.ToString();
-        m_scoresOfPlayers[1].text = b.ToString();
-        m_scoresOfPlayers[2].text = c.ToString();
-        m_scoresOfPlayers[3].text = d.ToString();
+        m_scoresOfPlayers[0].text = a.ToString() + "♥";
+        m_scoresOfPlayers[1].text = b.ToString() + "♥";
+        m_scoresOfPlayers[2].text = c.ToString() + "♥";
+        m_scoresOfPlayers[3].text = d.ToString() + "♥";
     }
 }
